5dbb7922e4fd154c7348915f83fda361245816d8
SkarlyCat <114200148+SkarlyCat@users.noreply.github.com>
2022-10-06 18:09:56 -0500

    UnityProject
    Add UnityFolder

---
+++ Videojuego/Assets/ScriptPrueba.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ScriptPrueba : MonoBehaviour
+{
+    string Nombre = "Player1";
+
+    public int giro;
+
+    public bool girar; 
+
+    void Start()
+
+    {
+        name = Nombre;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(girar == true)
+        transform.Rotate(0, giro, 0);
+        
+
+    }   
+}
